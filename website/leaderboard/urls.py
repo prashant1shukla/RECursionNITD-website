eeeaeaf1ff5b6a1f756d7f3d423a485d9a7ea44c
bishwajit <arin17bishwa@gmail.com>
2021-12-16 15:26:41 +0530

    initial commit

---
+++ website/leaderboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+import views
+
+app_name = "leaderboard"
+
+urlpatterns = [
+
+]
