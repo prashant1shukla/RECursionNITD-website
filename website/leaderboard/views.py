9b314364fd40a1fd94fc99309c94b0e4b871730b
bishwajit <arin17bishwa@gmail.com>
2021-12-18 01:44:12 +0530

    added a leaderboard home view just for testing

--- website/leaderboard/views.py
+++ website/leaderboard/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .leaderboard_scheduler.utils import CFUpdater
+
 
 # Create your views here.
+
+def view(request):
+    CFUpdater()
+    return HttpResponse('hi')
