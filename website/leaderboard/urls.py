b8a978e7f5f990666afa1d31cec20e0a2475c42f
bishwajit <arin17bishwa@gmail.com>
2021-12-18 01:47:09 +0530

    updated urls.py

--- website/leaderboard/urls.py
+++ website/leaderboard/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-import views
-
+from .views import (
+    view
+)
 app_name = "leaderboard"
 
 urlpatterns = [
-
+    path('', view, name='leaderboard_home'),
 ]
