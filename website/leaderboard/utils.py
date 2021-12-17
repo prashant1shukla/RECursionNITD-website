bcf2c28bde47612a884cf13cc44f750b3ab813cf
bishwajit <arin17bishwa@gmail.com>
2021-12-18 01:45:23 +0530

    added some proper functionality to utils.py

--- website/leaderboard/utils.py
+++ website/leaderboard/utils.py
@@ -1,6 +1,24 @@
+import time
+
+from .models import Codeforces
+from .leaderboard_scheduler.apis import CodeforcesAPI
+
+
 def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+    print('starting main()')
+    forces = CodeforcesAPI()
+    users = forces.getDataFromFile()
+    current_time = time.time()
+    for user in users:
+        handle = user['handle']
+        is_active = (current_time - (user.get('lastOnlineTimeSeconds', 0))) < (31 * 24 * 3600)
+        obj, created = Codeforces.objects.get_or_create(
+            username=handle,
+            first_name=user.get('firstName', ''),
+            last_name=user.get('lastName', ''),
+            current_rating=user.get('rating', 0),
+            max_rating=user.get('maxRating', 0),
+            is_active=is_active
+        )
+        if created:
+            print(obj)
