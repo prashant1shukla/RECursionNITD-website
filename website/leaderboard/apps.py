5a1e5c7be08a1079530f751290e2141682aa8b51
bishwajit <arin17bishwa@gmail.com>
2021-12-18 01:48:29 +0530

    made ready() for apscheduler functionalities

--- website/leaderboard/apps.py
+++ website/leaderboard/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class LeaderboardConfig(AppConfig):
     name = 'leaderboard'
+
+    def ready(self):
+        print('starting the update')
+        from .leaderboard_scheduler import utils
+        utils.startUpdate()
