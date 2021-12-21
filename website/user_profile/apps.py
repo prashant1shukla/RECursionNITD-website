b6a8a035925b69403c7d78fc0c130f31f27078cb
bishwajit <arin17bishwa@gmail.com>
2021-12-22 02:15:19 +0530

    initial commit for scheduling on ready

--- website/user_profile/apps.py
+++ website/user_profile/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class UserProfileConfig(AppConfig):
     name = 'user_profile'
+
+    def ready(self):
+        # print('starting ready() func')
+        from .utils import DPAddScheduler
+        DPAddScheduler()
