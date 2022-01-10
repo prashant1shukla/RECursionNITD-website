c00bd47ea41d88bd40fadf6aeaf2663ada9e3cb0
bishwajit <arin17bishwa@gmail.com>
2022-01-11 01:36:06 +0530

    made and registered the StopStalk model

--- website/leaderboard/models.py
+++ website/leaderboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class StopStalk(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    stopStalk_id = models.PositiveIntegerField(unique=True)
+    codechef_url = models.URLField(blank=True)
+    codeforces_url = models.URLField(blank=True)
+    spoj_url = models.URLField(blank=True)
+    codechef_solved = models.PositiveIntegerField(default=0)
+    codeforces_solved = models.PositiveIntegerField(default=0)
+    spoj_solved = models.PositiveIntegerField(default=0)
+    total_solved = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['username', ]
+        verbose_name_plural = 'StopStalkProfiles'
